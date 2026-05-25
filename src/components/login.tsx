af0afcc9765df07137350aa165beaef9c2c9124f
Daniel Marcus <dm@danielmarcus.de>
2026-05-25 19:20:18 +0200

    fix(login): show btn when token expired

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -4,11 +4,13 @@ import { useAuth0 } from "@auth0/auth0-react"
 
 export function Login() {
   const pathname = usePathname()
-  const { isAuthenticated, loginWithRedirect } = useAuth0()
+  const { isAuthenticated, loginWithRedirect, error } = useAuth0()
   useEffect(() => {
     if (pathname === "/login") loginWithRedirect()
   }, [pathname, loginWithRedirect])
-  return isAuthenticated || !process.env.NEXT_PUBLIC_AUTH0_DOMAIN ? null : (
+  if ((isAuthenticated && !error) || !process.env.NEXT_PUBLIC_AUTH0_DOMAIN)
+    return null
+  return (
     <button
       className="w-full p-2 bg-gray-100 dark:bg-gray-800"
       onClick={() => loginWithRedirect()}
